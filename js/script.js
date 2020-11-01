deececa156edb69c61931be3bbc71cce884589fc
Abdul Samad <Samk8778@yahoo.com>
2020-11-01 20:37:23 +0530

    :zap: Optimized images and code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-window.addEventListener('load',loading(),false);
-    function loading(){
-        var main = document.getElementById("main");
-        var loader = document.getElementById("loader");
-        main.style.display = "block";
-        loader.style.display = "none";
-    }
+window.addEventListener('load', loading(), false);
+function loading() {
+	var main = document.getElementById('main');
+	var loader = document.getElementById('loader');
+	main.style.display = 'block';
+	loader.style.display = 'none';
+}
